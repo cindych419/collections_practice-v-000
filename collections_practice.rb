c80b8692ccf5625ae0da4728254daf6e3be417d4
cindych419@gmail.com <cindych419@gmail.com>
2018-05-03 18:21:50 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,6 +10,8 @@ def sort_array_char_count(character)
   character.sort_by!{|word| word.length}
 end
 
-def swap_elements(array)
-  array.insert(1,array[2])
+def swap_elements(array, index, destination_index)
+  destination_index = array.insert(1,array[2])
+  destination_index.delete(array[3])
+  destination_index
 end
